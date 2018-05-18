bd9c2ba747a56f5b51a7d450c01ae4cef20af8c1
toussam <mathias.toussaint@outlook.com>
2018-05-18 16:01:00 +0200

    Reproduced the issues.

---
+++ SfDateTimeRangeNavigatorSampleIssues/SfDateTimeRangeNavigatorSampleIssues/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SfDateTimeRangeNavigatorSampleIssues.ViewModels
+{
+    class MainPageViewModel
+    {
+        #region Private Properties
+
+        private DateTime maximum;
+        private DateTime minimum;
+
+        #endregion
+
+        #region Public Properties
+
+        public DateTime Maximum { get { return maximum; } set { maximum = value; } }
+        public DateTime Minimum { get { return minimum; } set { minimum = value; } }
+
+        #endregion
+
+        #region Constructors
+
+        public MainPageViewModel(INavigation navigation)
+        {
+            var year = DateTime.Now.Year;
+            minimum = new DateTime(year, 1, 1);
+            maximum = new DateTime(year, 12, 31);
+        }
+
+        #endregion
+    }
+}
---
+++ SfDateTimeRangeNavigatorSampleIssues/SfDateTimeRangeNavigatorSampleIssues/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SfDateTimeRangeNavigatorSampleIssues.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SfDateTimeRangeNavigatorSampleIssues.Views
+{
+#if !DEBUG
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel context;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            context = new MainPageViewModel(Navigation);
+            BindingContext = context;
+        }
+    }
+}
